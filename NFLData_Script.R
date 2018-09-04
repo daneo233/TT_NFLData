ae6c391d8249c89f14badeaac502495163d7dc69
Dane <dane.hager@gmail.com>
2018-09-03 20:57:37 -0500

    initial commit

---
+++ NFLData_Script.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+data <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2018-08-28/nfl_2010-2017.csv")
+head(data)
+RushData %>% ggplot() +
+  geom_point(aes(x=rush_avg, y=rec_avg))
+RushData <- filter(data, data$rush_avg>4)
+data$position
+summarize(data$position)
+head(data)
+GroupedData <-group_by(data, team, game_year) %>% 
+  summarize(rush_yds=sum(rush_yds, na.rm=TRUE), rush_avg=sum(rush_yds, na.rm=TRUE)/sum(rush_att, na.rm=TRUE), rush_tds=sum(rush_tds, na.rm=TRUE), rec_yds=sum(rec_yds, na.rm=TRUE), rec_avg=sum(rec_yds, na.rm=TRUE)/sum(rec, na.rm=TRUE), rec_tds=sum(rec_tds, na.rm=TRUE))
+head(GroupedData)
+GroupedData  %>%  ggplot(aes(x=rush_avg, y=rec_avg)) +
+  geom_point()+
+  geom_text(mapping=aes(label=team))
+GroupedData %>% filter(game_year=="2016") %>%
+  ggplot(aes(x=rush_avg, y=rec_avg)) +
+  geom_text(mapping=aes(label=team))
+GroupedData %>% mutate(sum_avg=rush_avg+rec_avg) %>%
+  filter(team=="GB") %>%
+  ggplot(aes(x=game_year, y=sum_avg)) +
+  geom_line()
